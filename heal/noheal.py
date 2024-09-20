51294a666c5b14177876dd695a9ea79865be9bdb
Bowhza <bogtank2003@gmail.com>
2024-09-20 13:42:41 -0600

    Updated fullheal and noheal scripts

--- heal/noheal.py
+++ heal/noheal.py
@@ -1,9 +1,9 @@
 import pyautogui
 import pydirectinput
-
-import threading
 import logging
 import time
+
+from tools.image_scale import locate_image_on_screen
 
 log = logging.getLogger(__name__)
 
@@ -11,22 +11,15 @@ log = logging.getLogger(__name__)
 ImageNotFound = pyautogui.ImageNotFoundException
 
 
-def heal():
+def heal(scale_factor=1):
     while True:
         try:
-            if pyautogui.locateOnScreen('images/healskip.png', grayscale=True, confidence=0.75) is not None:
-                pydirectinput.click(1056, 574)
-                logging.info("heal skip detected")
-                time.sleep(1.5)
-        except ImageNotFound:
-            pass
-
-        try:
-            if pyautogui.locateOnScreen('images/dead.png', confidence=0.75) is not None:
+            if locate_image_on_screen('dead.png', scale_factor, confidence=0.75) is not None:
                 logging.info("Died or MIA")
-                next_img = pyautogui.locateOnScreen('images/next.png', grayscale=True, confidence=0.7)
+                next_img = locate_image_on_screen('next.png', scale_factor, confidence=0.7)
                 for x in range(5):
-                    pydirectinput.click(next_img.left + 20, next_img.top + 20)
-                    time.sleep(1)
+                    if next_img is not None:
+                        pydirectinput.click(next_img.left + 20, next_img.top + 20)
+                        time.sleep(1)
         except ImageNotFound:
             pass
